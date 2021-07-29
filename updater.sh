945e5ba1e1c06b55566cb59492f9afa5afc262c5
unknown <todd@hypernetlabs.io>
2021-07-29 14:34:44 -0700

    update

--- updater.sh
+++ updater.sh
@@ -14,15 +14,15 @@ if test -e /home/galileo/.galileo-ide/current_ide_version.txt; then
          https://api.github.com/repos/GoHypernet/Galileo-IDE/releases/latest | \
          grep "browser_download_url" |\
          sed 's/"browser_download_url": "//g' | sed 's/"//g' | sed -r 's/\s+//g' | sed ':a;N;$!ba;s/\n//g' > \
-         download_url.txt
-       echo "Downlaoding $(cat download_url.txt)"
-       curl -L $(cat download_url.txt) > /home/galileo/.galileo-ide/galileo_ide.tar.gz
+         /tmp/download_url.txt
+       echo "Downloading $(cat /tmp/download_url.txt)"
+       curl -L $(cat /tmp/download_url.txt) > /home/galileo/.galileo-ide/galileo_ide.tar.gz
        tar -xvf /home/galileo/.galileo-ide/galileo_ide.tar.gz -C /home/galileo/.galileo-ide
        rm /home/galileo/.galileo-ide/galileo_ide.tar.gz
        nohup supervisorctl restart galileo-ide > /tmp/nohup.out
    }
 
-   cmp --silent /home/galileo/.galileo-ide/latest_ide_version.txt /home/galileo/.galileo-ide/current_ide_version.txt && \
+   diff -w --strip-trailing-cr /home/galileo/.galileo-ide/latest_ide_version.txt /home/galileo/.galileo-ide/current_ide_version.txt && \
      echo '### No Update Detected ###' || \
      download_update
 else
